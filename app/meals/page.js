b8ada6656dba686629cdb67a975b538a5d5ba4fb
vlk2022 <volodymyrkostiuk2021@gmail.com>
2024-07-02 00:32:26 +0300

    created db

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,7 +1,33 @@
-export default function MealsPage() {
+import Link from 'next/link';
+import {getMeals} from "@/lib/meals";
+
+import classes from './page.module.css';
+import MealsGrid from '../../components/meals/meals-grid';
+
+
+export default async function MealsPage() {
+    const meals = await getMeals();
+
+
     return (
-        <main>
-            MealsPage
-        </main>
+        <>
+            <header className={classes.header}>
+                <h1>
+                    Delicious meals, created{' '}
+                    <span className={classes.highlight}>by you</span>
+                </h1>
+                <p>
+                    Choose your favorite recipe and cook it yourself. It is easy and fun!
+                </p>
+                <p className={classes.cta}>
+                    <Link href={"/meals/share"}>
+                        Share Your Favorite Recipe
+                    </Link>
+                </p>
+            </header>
+            <main className={classes.main}>
+                <MealsGrid meals={meals} />
+            </main>
+        </>
     )
 }
