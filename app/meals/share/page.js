d134b05dca252d48afa9828dc0c7b81ec8c4a8b9
vlk2022 <volodymyrkostiuk2021@gmail.com>
2024-07-08 14:18:58 +0300

    server actions

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -2,6 +2,7 @@ import ImagePicker from "@/components/meals/image-picker";
 import {shareMeal} from "@/lib/actions";
 
 import classes from './share.module.css';
+import MealsFormSubmit from "@/components/meals/meals-form-submi";
 
 
 export default function ShareMealPage() {
@@ -48,7 +49,7 @@ export default function ShareMealPage() {
                     <ImagePicker label="Your image" name="image" />
 
                     <p className={classes.actions}>
-                        <button type="submit">Share Meal</button>
+                        <MealsFormSubmit/>
                     </p>
                 </form>
             </main>
