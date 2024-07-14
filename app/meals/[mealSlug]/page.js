bf9bad024a2261bc11aa13d1b72f42a7c4a4e8cb
vlk2022 <volodymyrkostiuk2021@gmail.com>
2024-07-14 16:21:33 +0300

    server actions

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,79 +1,23 @@
-// import Image from 'next/image';
-// import { notFound } from 'next/navigation';
-//
-// import { getMeal } from '@/lib/meals';
-// import classes from './page.module.css';
-//
-//
-// export async function generateMetadata({ params }) {
-//     const meal = await getMeal(params.mealSlug);
-//
-//     if (!meal) {
-//         notFound();
-//     }
-//
-//     return {
-//         title: meal.title,
-//         description: meal.summary,
-//     };
-// }
-//
-//
-// export default async function MealDetailsPage({ params }) {
-//     const meal = await getMeal(params.mealSlug);
-//
-//     if (!meal) {
-//         notFound();
-//     }
-//
-//     meal.instructions = meal.instructions.replace(/\n/g, '<br />');
-//
-//
-//     return (
-//         <>
-//             <header className={classes.header}>
-//                 <div className={classes.image}>
-//                     <Image src={meal.image} alt={meal.title} fill />
-//                 </div>
-//                 <div className={classes.headerText}>
-//                     <h1>{meal.title}</h1>
-//                     <p className={classes.creator}>
-//                         by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
-//                     </p>
-//                     <p className={classes.summary}>{meal.summary}</p>
-//                 </div>
-//             </header>
-//             <main>
-//                 <p
-//                     className={classes.instructions}
-//                     dangerouslySetInnerHTML={{
-//                         __html: meal.instructions,
-//                     }}
-//                 ></p>
-//             </main>
-//         </>
-//     );
-// }
-
-
 import Image from 'next/image';
 import { notFound } from 'next/navigation';
 
 import { getMeal } from '@/lib/meals';
 import classes from './page.module.css';
 
-// export async function generateMetadata({ params }) {
-//     const meal = await getMeal(params.mealSlug);
-//
-//     if (!meal) {
-//         notFound();
-//     }
-//
-//     return {
-//         title: meal.title,
-//         description: meal.summary,
-//     };
-// }
+
+export async function generateMetadata({ params }) {
+    const meal = await getMeal(params.mealSlug);
+
+    if (!meal) {
+        notFound();
+    }
+
+    return {
+        title: meal.title,
+        description: meal.summary,
+    };
+}
+
 
 export default async function MealDetailsPage({ params }) {
     const meal = await getMeal(params.mealSlug);
@@ -83,6 +27,7 @@ export default async function MealDetailsPage({ params }) {
     }
 
     meal.instructions = meal.instructions.replace(/\n/g, '<br />');
+
 
     return (
         <>
@@ -109,3 +54,7 @@ export default async function MealDetailsPage({ params }) {
         </>
     );
 }
+
+
+
+
